290b8df28010df12a19b02f34aefb94a1cc90150
VillamilA <angel.villamil@epn.edu.ec>
2024-05-29 19:44:34 -0500

    Se crea la clase con sus atributos y por ultimo el contructor

---
+++ src/Hotel.java
@@ -0,0 +1,16 @@
+//ANGEL VILLAMIL
+    public class Hotel {
+        // Atributos
+        private String nombre;
+        private String direccion;
+        private int estrellas;
+        // Constructor por defecto o vacio
+        public Hotel() {}
+        // Realizamos un constructor con parametros
+        public Hotel(String nombre, String direccion, int estrellas) { //
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.estrellas = estrellas;
+        }
+
+    }
